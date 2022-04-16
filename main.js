499f851a1704671385af96e3350fb7fbf8581760
Arya <84271899+aryabhatta2009@users.noreply.github.com>
2022-04-16 11:28:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var menu_list_array=[]; 
+menu_list_array = ["veg Margherita Pizza","Chicken Tandoori Pizza","Deluxe Veggie Pizza","Paneer Tikka Pizza","Veg Extravaganza Pizza"] 
+function getmenu(){
+document.getElementById("display_menu").innerHTML = htmldata;
+}  
+function add_item(){ 
+var htmldata ;
+var itemdocument.getElementById("add_item").value; 
+menu_list_array.sort(); 
+htmldata = "";}
